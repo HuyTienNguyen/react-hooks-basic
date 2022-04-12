e43df1d73a4eb6c2e93b712092630a1190943d22
HuyNT <tienhuy031001@gmail.com>
2022-04-12 20:41:52 +0700

    HuyNT40

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ColorBox from "./components/ColorBox";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "Nguyen tien Huy" },
+    { id: 2, title: "FPT software" },
+    { id: 3, title: "Ha Noi" },
+  ]);
+
+  function handleTodoClick(todo) {
+    const index = todoList.findIndex((x) => x.id === todo.id);
+    if (index < 0) return;
+    const todoNewList = [...todoList];
+    console.log(typeof todoNewList);
+    console.log(todoNewList);
+    todoNewList.splice(index, 1);
+    setTodoList(todoNewList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world!</h1>
+      <ColorBox />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
